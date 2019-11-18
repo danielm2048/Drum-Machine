9885774c32f4495fc8b20d1ca9c2c37a638ac5e4
danielm2048 <danielmimoun98@gmail.com>
2019-11-18 03:00:29 +0200

    Updates

---
+++ precache-manifest.0d28cb1eb15ad5fbe012fc0e444c478e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdfa7b50787e5aef31a35b6bd6a9905",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "d2a675b1a6f21cc4a261",
+    "url": "/Drum-Machine/static/css/main.b742cc0b.chunk.css"
+  },
+  {
+    "revision": "a4f74c2a9961c63a1c30",
+    "url": "/Drum-Machine/static/js/2.80df9afa.chunk.js"
+  },
+  {
+    "revision": "d2a675b1a6f21cc4a261",
+    "url": "/Drum-Machine/static/js/main.828ad04c.chunk.js"
+  },
+  {
+    "revision": "638726afcb1ca4a12c66",
+    "url": "/Drum-Machine/static/js/runtime-main.1bde2130.js"
+  },
+  {
+    "revision": "b9a08a5df24c9847056b3108756e5ee8",
+    "url": "/Drum-Machine/static/media/buzz.b9a08a5d.mp3"
+  },
+  {
+    "revision": "e226c23275e7c7b99c5bff7447bc5d57",
+    "url": "/Drum-Machine/static/media/clap.e226c232.mp3"
+  },
+  {
+    "revision": "04b83e3a26c32f750c9036769cadf86d",
+    "url": "/Drum-Machine/static/media/cymbal.04b83e3a.mp3"
+  },
+  {
+    "revision": "b44c174da2c6297578a01956c84fd9d2",
+    "url": "/Drum-Machine/static/media/flam.b44c174d.mp3"
+  },
+  {
+    "revision": "c704d11e9d3aedc664d7c5c1cac82f01",
+    "url": "/Drum-Machine/static/media/kick.c704d11e.mp3"
+  },
+  {
+    "revision": "c40b171b46e2c3b535585c405f8138fd",
+    "url": "/Drum-Machine/static/media/opHat.c40b171b.mp3"
+  },
+  {
+    "revision": "d1667d1c77106f5a347ae52631a8d058",
+    "url": "/Drum-Machine/static/media/shaker.d1667d1c.mp3"
+  },
+  {
+    "revision": "6c1c1abac15576be8cfc0a39cb22303d",
+    "url": "/Drum-Machine/static/media/snare.6c1c1aba.mp3"
+  },
+  {
+    "revision": "37c4a0e13a8f3c5e8e1427c3090423ec",
+    "url": "/Drum-Machine/static/media/tom.37c4a0e1.mp3"
+  }
+]);
